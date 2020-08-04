d66f07b3c99cb1d8456c4411482aa8cb97ce3286
Matthew <mt.paragon5@gmail.com>
2020-08-04 09:53:10 -0400

    implicit variable tips and dob app

--- code/1.HelloWorld/Program.cs
+++ code/1.HelloWorld/Program.cs
@@ -77,13 +77,34 @@ namespace HelloWorld
 
       #endregion value types
 
-      // methods
-      #region methods
+      // var / suffix assignment
+      #region using var and suffix assignment
+
+      var mySByte = (sbyte)-0b01;         // sbyte -> -128 to 127 -> or denoted by 0b... in C#7 
+      var myByte = (byte)0b0101;          // byte -> 0 to 255 -> or denoted by 0b... in C#7 
+      var myShort = (short)1;             // short -> -32,768 to 32,767
+      var myUShort = (ushort)1;           // ushort -> 0 to 65,535
+      var myInt = 1;                      // int -> -2,147,483,648 to 2,147,483,647 -> this is the default
+      var myUInt = (uint)1;               // uint -> 0 to 4,294,967,295
+      var myLong = 1L;                    // long -> -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+      var myULong = 1UL;                  // ulong ->  0 to 18,446,744,073,709,551,615
+
+      // floats
+      var f = 1.1F;                       // float -> 6-9 digits (32 bits)
+      var d = 1.1;                        // double -> 15-17 digits (64 bits) - this is the default
+      var dec = 1.1M;                     // decimal -> 28-29 digits (128 bits)
 
 
 
       #endregion methods
 
+
+
+      #region parsing
+
+
+
+      #endregion parsing
     }
   }
 }
---
+++ code/3.BirthdayCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace _3.BirthdayCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IntroducePaul();
+      var usersDateOfBirth = AskForDateOfBirth();
+    }
+
+    public static void IntroducePaul()
+    {
+      Console.WriteLine("Hi, I'm Paul!\nI bet I can guess what day of the week you were born on! C'mon I'll show you!");
+    }
+
+    public static DateTimeOffset AskForDateOfBirth()
+    {
+      Console.WriteLine($"What's your date of birth? ({CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern})");
+
+      var userInput = Console.ReadLine();
+
+      var parsedDate = DateTimeOffset.MinValue;
+
+      var succeeded = DateTimeOffset.TryParse(userInput, out parsedDate);
+
+      if (succeeded)
+      {
+        return parsedDate;
+      }
+      else
+      {
+        Console.WriteLine($"Oops, you entered an invalid date...\n");
+
+        return AskForDateOfBirth();
+      }
+    }
+  }
+}
